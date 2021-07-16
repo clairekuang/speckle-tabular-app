71eebc1dd8e8deacca7f458f18be34814c00f810
Claire Kuang <kuang.claire@gmail.com>
2021-07-16 19:10:16 +0100

    added commit url parsing

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,151 +1,82 @@
 <template>
   <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
-
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
-
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
-        </p>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          What's next?
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Important Links
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Ecosystem
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
-      </v-col>
+    <v-row>
+      <v-text-field
+        label="commit url"
+        flat
+        rounded
+        background-color="light-blue lighten-5"
+        solo
+        v-model="URL">
+        <template #append>
+            <v-btn
+                rounded
+                color="blue"
+                @click="getObjects">
+            get
+            </v-btn>
+        </template>
+      </v-text-field>
     </v-row>
+     
   </v-container>
 </template>
 
 <script>
   export default {
     name: 'HelloWorld',
-
     data: () => ({
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader',
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify',
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify',
-        },
-      ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com',
-        },
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com',
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuejs.com/vuetify',
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs',
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify',
-        },
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer',
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-        },
-      ],
+      URL: 'https://speckle.xyz/streams/5dfbeb49c9/commits/22bb89b5b8'
     }),
+    methods: {
+      async getObjects() {
+
+        // get the stream and commit id from the url
+        const url = new URL(this.URL)
+        var pathArray = url.pathname.split('/')
+        const streamId = pathArray[2]
+        const commitId = pathArray[4]
+
+        // create a query for graphql for all children of the commit
+        const query = this.buildQuery(streamId, commitId)
+        console.log(query)
+
+        // fetch
+        var server = url.protocol.concat("//").concat(url.host)
+        console.log(server)
+        let rawRes = await fetch( new URL( '/graphql', server), {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify( {
+            query: query,
+            variables: { }
+          } )
+        } )
+
+        let res = await rawRes.json()
+        let obj = res.data.stream.object
+        console.log(obj)
+
+      },
+
+      buildQuery( streamId, objectId) {
+      return `
+        query( $mySelect: [String!], $myQuery: [JSONObject!]) {
+          stream( id: "${streamId}" ) {
+            object( id: "${objectId}" ) {
+              data
+              children( 
+                objects{
+                  data
+                }
+              }
+            }
+          }
+        }
+      `
+      }
+    }
   }
 </script>
